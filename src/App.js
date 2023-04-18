40ca8edf6e3037602d75c9bfa03d539848ecc2ac
Saman <samangithoseinpour@gmail.com>
2023-04-18 20:33:43 +0330

    feat--Table-component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import ModalPage from './pages/ModalPage';
+import TablePage from './pages/TablePage';
 
 const App = () => {
   return (
@@ -21,6 +22,9 @@ const App = () => {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/table">
+          <TablePage />
         </Route>
       </div>
     </div>
---
+++ src/components/Table.js
@@ -0,0 +1,32 @@
+const Table = ({ tableData, tableConfig, keyFn }) => {
+  const renderedHeaders = tableConfig.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedFruits = tableData.map((rowData) => {
+    const renderedCells = tableConfig.map((cells) => {
+      return (
+        <td className="p-2" key={cells.label}>
+          {cells.render(rowData)}
+        </td>
+      );
+    });
+
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedFruits}</tbody>
+    </table>
+  );
+};
+
+export default Table;
